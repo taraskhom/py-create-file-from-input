89a37ad94f9a281e3aefe2082e163cab8c6a0590
taraskhom <khomenkotaras1305@gmail.com>
2024-12-15 15:03:29 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 def main() -> None:
-    name_of_the_file = input("Enter name of the file: ")
-    with open(f"{name_of_the_file}.txt", "a") as saving_file:
+    name_of_file = input("Enter name of the file: ")
+    with open(f"{name_of_file}.txt", "a") as saving_file:
         while True:
-            new_line_of_file = input("Enter new line of content: ")
-            if new_line_of_file != "stop":
-                saving_file.write(f"{new_line_of_file}\n")
+            new_line_input = input("Enter new line of content: ")
+            if new_line_input != "stop":
+                saving_file.write(f"{new_line_input}\n")
             else:
                 break
 
